e2ef628453234f0e3321d6566a4c8a5e3a77e6df
RichieGarafola-IberiaAdvisory <135731462+RichieGarafola-IberiaAdvisory@users.noreply.github.com>
2025-07-29 08:46:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Add Microsoft keys and repo for msodbcsql18
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
 # Update system packages
 apt-get update
 
